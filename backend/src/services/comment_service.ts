6f86f7fbc23b9e545c85cdf9647a2a64051c2faa
snira <snirashwal@gmail.com>
2024-02-03 17:47:04 +0200

    Failed like and dislike tests

--- backend/src/services/comment_service.ts
+++ backend/src/services/comment_service.ts
@@ -1,18 +1,10 @@
-
 import User from "../models/user_model";
 import { IComment } from "../models/comment_model";
-import {mongo} from 'mongoose';
 
 class CommentService {
-    
-    public convertToIdObject(id: string){
-        return new mongo.ObjectId(id);
-    }
-
-    async getOwnerObj(comment: IComment) {
-        return await User.findById(comment.ownerId);
-    }
-    
+  async getOwnerObj(comment: IComment) {
+    return await User.findById(comment.ownerId);
+  }
 }
 
 export default new CommentService();
