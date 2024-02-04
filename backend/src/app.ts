47aa5e4b04b3b71219d2714a87e3366fb1226aeb
snira <snirashwal@gmail.com>
2024-02-04 13:43:16 +0200

    Export image file folder to .env

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -24,7 +24,10 @@ const initApp = (): Promise<Express> => {
       .then(() => {
         const app = express();
         app.use(cors());
-        app.use("/uploads", express.static("C:/PawPals/uploads"));
+        app.use(
+          "/uploads",
+          express.static(process.env.UPLOAD_IMAGE_FILES_DIRECTORY)
+        );
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({ extended: true }));
         app.use("/user", userRoute);
